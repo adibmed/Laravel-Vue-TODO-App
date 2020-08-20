87452e8aedbad062d6165cb676dc540c6b9eaa8f
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-08-20 20:58:33 +0000

    add sqlite db connection

--- database/migrations/2020_08_20_204515_create_todos_table.php
+++ database/migrations/2020_08_20_204515_create_todos_table.php
@@ -15,6 +15,8 @@ class CreateTodosTable extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->boolean('completed')->default(0);
             $table->timestamps();
         });
     }
