f4a65048ba61b502156970e28dfd3dc4d12eadce
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-30 22:55:54 +0000

    Issue: heroku conf + api

--- public/js/app.js
+++ public/js/app.js
@@ -51339,8 +51339,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/adibe/TODO-App-Laravel/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/adibe/TODO-App-Laravel/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/adibe/Projects/TODO-App-Laravel/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/adibe/Projects/TODO-App-Laravel/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
